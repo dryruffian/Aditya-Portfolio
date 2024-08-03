ea8327c746f719f60c938cec14d161e487cb8e71
Aditya raj <88555779+dryruffian@users.noreply.github.com>
2024-08-03 21:41:03 +0530

    Added project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import PortfolioProjectCard from './PortfolioProjectCard';
 import CodeCartograhper from '../assets/Code Cartograhper.jpeg';
 import BERTneural from '../assets/BERT neural network.jpeg';
+import AWS from '../assets/AWS.jpeg';
 
 
 
@@ -14,6 +15,13 @@ const Projects = () => {
       image: BERTneural
     },
     {
+      title: "AWS API Documentation Scraper & Processor",
+      description: "This Python project automates the process of extracting and organizing information from AWS API documentation, making it easier to access and analyze AWS API data.",
+      skills: ["python","scrapy"],
+      link: "https://github.com/dryruffian/AWSEC2_API_ACTION_Scrape",
+      image: AWS
+    },
+    {
       title: "Code Cartographer UI",
       description: "This tool The tool extracts the contents of the uploaded archive, generates a directory tree, and displays the contents of text files while respecting .gitignore patterns and common ignore rules.",
       skills: ["python"],
@@ -25,7 +33,7 @@ const Projects = () => {
   return (
     <div className="my-3 text-white text-center w-full p-4 md:p-8 rounded-3xl bg-gradient-to-b from-white/20 to-white/10 backdrop-blur-md border-b border-white/20 shadow-lg relative">
       <h1 className="text-2xl font-bold mb-6 relative inline-block">
-        Projects
+        Personal Projects
         <div className="absolute bottom-0 left-0 w-full h-0.5 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500"></div>
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
